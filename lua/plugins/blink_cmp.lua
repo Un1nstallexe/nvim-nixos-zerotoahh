c0e80d1f94904838783a3632ae1b6c0de2ef5415
Kiryl Paulouski <kirr2156@gmail.com>
2026-02-22 18:32:53 +0100

    style: finally format everything

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -1,30 +1,30 @@
 return {
-  "blink.cmp",
-  -- Load when entering Insert Mode or Command Mode
-  event = { "InsertEnter", "CmdlineEnter" },
-  after = function()
-    require("blink.cmp").setup({
-      keymap = { preset = "enter" },
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = "mono"
-      },
-      completion = {
-        menu = {
-          draw = {
-            columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } }
-          }
-        },
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 200,
-        },
-      },
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
-      -- Optional: adjust signature help if you want it to pop up as you type
-      signature = { enabled = true }
-    })
-  end,
+	"blink.cmp",
+	-- Load when entering Insert Mode or Command Mode
+	event = { "InsertEnter", "CmdlineEnter" },
+	after = function()
+		require("blink.cmp").setup({
+			keymap = { preset = "enter" },
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+			completion = {
+				menu = {
+					draw = {
+						columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+					},
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 200,
+				},
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+			-- Optional: adjust signature help if you want it to pop up as you type
+			signature = { enabled = true },
+		})
+	end,
 }
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,19 +1,19 @@
 return {
-  "trouble.nvim",
-  keys = {
-    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
-  },
-  -- `after` runs exactly once, right after the plugin is added to the runtimepath
-  after = function()
-    local trouble = require("trouble")
-    trouble.setup({})
-    vim.keymap.set("n", "]q", function()
-      trouble.next({ skip_groups = true, jump = true })
-    end, { desc = "Next Trouble Item" })
+	"trouble.nvim",
+	keys = {
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+	},
+	-- `after` runs exactly once, right after the plugin is added to the runtimepath
+	after = function()
+		local trouble = require("trouble")
+		trouble.setup({})
+		vim.keymap.set("n", "]q", function()
+			trouble.next({ skip_groups = true, jump = true })
+		end, { desc = "Next Trouble Item" })
 
-    -- Jump to the previous item in the trouble list
-    vim.keymap.set("n", "[q", function()
-      trouble.prev({ skip_groups = true, jump = true })
-    end, { desc = "Previous Trouble Item" })
-  end,
+		-- Jump to the previous item in the trouble list
+		vim.keymap.set("n", "[q", function()
+			trouble.prev({ skip_groups = true, jump = true })
+		end, { desc = "Previous Trouble Item" })
+	end,
 }
