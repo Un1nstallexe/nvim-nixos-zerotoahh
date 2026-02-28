4201a6298b6a42d8065b3d888d3637960551d152
Kiryl Paulouski <kirr2156@gmail.com>
2026-02-28 22:20:38 +0100

    fix: correct neotest-playwright setup

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -4,7 +4,12 @@ return {
 		require("neotest").setup({
 			adapters = {
 				require("neotest-dotnet"),
-				require("neotest-playwright"),
+				require("neotest-playwright").adapter({
+					options = {
+						persist_project_selection = true,
+						enable_dynamic_test_discovery = true,
+					},
+				}),
 			},
 		})
 	end,
