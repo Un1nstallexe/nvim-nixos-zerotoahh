ba05bafa598d75d7f94400b6d5708ac92d98757a
Kiryl Paulouski <kirr2156@gmail.com>
2026-08-22 20:39:16 +0200

    feat: add a shortcut to disable format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,9 +1,34 @@
 return {
 	"conform.nvim",
 	-- Load only when you are about to save a file
-	event = { "BufWritePre" },
+	event = { "BufEnter" },
+	keys = {
+		{
+			"<leader>taf",
+			function()
+				if vim.g.disable_autoformat == nil then
+					vim.g.disable_autoformat = false
+				end
+				vim.g.disable_autoformat = not vim.g.disable_autoformat
+				if vim.g.disable_autoformat == true then
+					vim.notify("Formatting on save is disabled.")
+				else
+					vim.notify("Formatting on save is enabled")
+				end
+			end,
+			desc = "Toggle auto format",
+		},
+	},
 	after = function()
 		local conform = require("conform")
+		local format_if_allowed = function(bufnr)
+			if vim.g.disable_autoformat then
+				vim.notify("Skipping formatting: formatting on save is disabled.")
+				return nil
+			end
+
+			return { timeout_ms = 500, lsp_format = "fallback", bufnr = bufnr }
+		end
 
 		conform.setup({
 			formatters_by_ft = {
@@ -22,17 +47,7 @@ return {
 				cs = { "csharpier" },
 				kt = { "ktfmt" },
 			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-		})
-
-		-- Force format on the very first save that triggered the plugin load
-		conform.format({
-			bufnr = vim.api.nvim_get_current_buf(),
-			timeout_ms = 500,
-			lsp_fallback = true,
+			format_on_save = format_if_allowed,
 		})
 	end,
 }
